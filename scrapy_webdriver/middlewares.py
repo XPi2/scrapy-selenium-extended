595d1096c6f4a75da8d567969464946ae12a834f
XPi2 <uxiopineiro@gmail.com>
2021-02-22 00:11:15 +0100

    Feat: Add base capabilities to SeleniumMiddleware
    - Add smart driver selection & driver capabilities formatting
    - Improve tests.

--- scrapy_webdriver/middlewares.py
+++ scrapy_webdriver/middlewares.py
@@ -1,4 +1,7 @@
 """Module for SeleniumMiddleware scrapy middleware."""
+from importlib import import_module
+from types import SimpleNamespace
+
 from scrapy import http
 from scrapy import signals
 from scrapy.crawler import Crawler
@@ -7,6 +10,8 @@ from scrapy.settings import Settings
 from scrapy.spiders import Spider
 from scrapy.utils.python import to_bytes
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from twisted.web.client import ResponseFailed
 
 from scrapy_webdriver.utils import get_from_settings
 
@@ -15,13 +20,37 @@ class SeleniumMiddleware:
     """Scrapy middleware to handle requests using selenium."""
 
     command_executor: str
+    driver_executable_path: str
+    capabilities: dict = {}
+    browser_name: str = ""
+    driver_options: list = []
+
+    _data = SimpleNamespace()
 
     def __init__(self, settings: Settings):
         """Initialize the selenium webdriver."""
-        for key, type_ in self.__annotations__.items():
-            self._set_attribute(settings, type_, key)
-        
-        self.get_driver()
+        # TODO: Option to local or remote driver
+        if "SELENIUM_COMMAND_EXECUTOR" in settings:
+            self._set_attribute(settings, str, "command_executor")
+        elif "SELENIUM_DRIVER_EXECUTABLE_PATH" in settings:
+            self._set_attribute(settings, str, "driver_executable_path")
+        else:
+            raise NotConfigured(
+                "One of (SELENIUM_DRIVER_EXECUTABLE_PATH, SELENIUM_COMMAND_EXECUTOR) must be provided"
+            )
+
+        self._set_attribute(settings, dict, "capabilities")
+        self._set_attribute(settings, list, "browser_name")
+        self._set_attribute(settings, list, "driver_options")
+
+        self._data.capabilities = self.capabilities
+
+        if self.browser_name:
+            driver_options = self._get_driver_options(self.browser_name)
+            for argument in self.driver_options:
+                driver_options.add_argument(argument)
+            self._data.capabilities.update(driver_options.to_capabilities())
+        # TODO: Raise warning if driver_options but not browser_name
 
     @classmethod
     def from_crawler(cls, crawler: Crawler) -> "SeleniumMiddleware":
@@ -31,37 +60,78 @@ class SeleniumMiddleware:
         crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
 
         return middleware
-    
+
     def process_request(self, request: http.Request, spider: Spider) -> http.Response:
         """Process a request using the selenium driver."""
-        driver = self.get_driver()
-
-        driver.get(request.url)
-        url = driver.current_url
-        body = to_bytes(driver.page_source)
+        try:
+            driver = self.get_driver()
+            driver.get(request.url)
+            url = driver.current_url
+            body = to_bytes(driver.page_source)
+        except WebDriverException as e:
+            raise ResponseFailed(f"WebDriverException {e}")
 
         return http.HtmlResponse(url=url, body=body, encoding="utf-8", request=request)
 
     def spider_closed(self) -> None:
         """Shutdown the driver when spider is closed."""
         self.driver.quit()
-    
-    def get_driver(self):
-        """Get remote driver."""
+
+    def get_driver(self) -> webdriver.Remote:
+        """Get driver in use, if is not available request new one."""
         try:
-            self.driver # TODO: Check for something reliable
+            driver = self.driver
         except AttributeError:
-            driver = webdriver.Remote(
-                command_executor=self.command_executor
-            )
+            driver = self._get_driver(self)
             self.driver = driver
-        return self.driver
-        
-    def _set_attribute(self, settings: Settings, type_: type, key: str) -> None:
-        default = getattr(self, key, None)  # Default value
+        return driver
+
+    def get_local_driver(self) -> webdriver.Remote:
+        """Get local driver."""
+        browser_name = self._data.capabilities["browserName"]
+        driver_class = self._get_driver_class(browser_name)
+        return driver_class(
+            executable_path=self._data.executable_path,
+            capabilities=self._data.capabilities,
+        )
+
+    def get_remote_driver(self) -> webdriver.Remote:
+        """Get remote driver."""
+        return webdriver.Remote(
+            command_executor=self.command_executor,
+            desired_capabilities=self._data.capabilities,
+        )
+
+    def _get_driver(self) -> webdriver.Remote:
+        if self.command_executor is not None:
+            return self.get_remote_driver()
+        elif self.driver_executable_path is not None:
+            return self.get_local_driver()
+        raise NotConfigured(
+            "One of (SELENIUM_DRIVER_EXECUTABLE_PATH, SELENIUM_COMMAND_EXECUTOR) must be provided"
+        )
+
+    def _get_driver_class(self, browser_name: str) -> callable:
+        webdriver_base_path = self._webdriver_base_path(browser_name)
+        driver_class_module = import_module(f"{webdriver_base_path}.webdriver")
+        driver_class = getattr(driver_class_module, "WebDriver")
+        return driver_class
+
+    def _get_driver_options(self, browser_name: str) -> object:
+        # TODO: Try except (driver name not supported)
+        webdriver_base_path = self._webdriver_base_path(browser_name)
+        driver_options_module = import_module(f"{webdriver_base_path}.options")
+        driver_options_class = getattr(driver_options_module, "Options")
+        return driver_options_class()
+
+    def _webdriver_base_path(self, browser_name: str) -> str:
+        return f"selenium.webdriver.{browser_name}"
+
+    def _set_attribute(self, settings: Settings, type_: type, key: str, default=None) -> None:
+        default_ = getattr(self, key, default)  # Default value
         setting_key = f"selenium_{key}".upper()
-        if not default and setting_key not in settings:
+        if default_ is None and setting_key not in settings:
             raise NotConfigured(f"{setting_key} has to be set.")
 
-        value = get_from_settings(settings, type_, setting_key, default)
+        value = get_from_settings(settings, type_, setting_key, default_)
         setattr(self, key, value)
--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -1,7 +1,10 @@
 """Module for testing ``scrapy_webdriver`` middlewares."""
 from unittest import TestCase
+from unittest.mock import Mock
 
-from scrapy import Crawler
+from scrapy import Spider
+from scrapy.utils.test import get_crawler
+from selenium import webdriver
 
 from scrapy_webdriver import middlewares
 
@@ -9,41 +12,63 @@ from scrapy_webdriver import middlewares
 class BaseScrapyTestCase(TestCase):
     """Base test case."""
 
-    class SimpleSpider(scrapy.Spider):
-        name = 'simple_spider'
-        allowed_domains = ['python.org']
-        start_urls = ['http://python.org']
+    class SimpleSpider(Spider):
+        name = "simple_spider"
+        allowed_domains = ["python.org"]
+        start_urls = ["http://python.org"]
 
         def parse(self, response):
             pass
-    
+
     @classmethod
     def setUpClass(cls):
         cls.settings = {}
         cls.spider_class = cls.SimpleSpider
-    
+
 
 class SeleniumMiddlewareTestCase(BaseScrapyTestCase):
     """Test case for SeleniumMiddleware."""
 
     @classmethod
     def setUpClass(cls):
+        """Set up base class and update secrets before any test."""
         super().setUpClass()
+        cls.settings.update(
+            {
+                "SELENIUM_COMMAND_EXECUTOR": "http://localhost:4444",
+                "SELENIUM_CAPABILITIES": webdriver.DesiredCapabilities.FIREFOX,
+            }
+        )
 
-        cls.settings.update({
-            'SELENIUM_COMMAND_EXECUTER': ''
-        })
+    def test_selenium_from_crawler(self):
+        """Test initialization of driver from crawler."""
+        crawler = self._mock_crawler(self.spider_class, self.settings)
+        selenium_middleware = middlewares.SeleniumMiddleware.from_crawler(crawler)
 
-        crawler = cls._mock_crawler()
+        self.assertIsNotNone(selenium_middleware.command_executor)
 
-        cls.selenium_middleware = middlewares.SeleniumMiddleware.from_crawler(crawler)
+    def test_selenium_spider_close(self):
+        """Test that driver closes on spider close."""
+        crawler = self._mock_crawler(self.spider_class, self.settings)
+        selenium_middleware = middlewares.SeleniumMiddleware.from_crawler(crawler)
 
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
+        mock_driver = Mock()
+        selenium_middleware.driver = mock_driver
+        selenium_middleware.spider_closed()
 
-        cls.selenium_middleware.driver.quit()
+        mock_driver.quit.assert_called_once()
 
-    @classmethod
-    def _mock_crawler(cls):
-        return Crawler(spidercls=cls.spider_class, settings=cls.settings)
+    def _mock_crawler(self, spider, settings=None):
+        class MockedDownloader(object):
+            slots = {}
+
+        class MockedEngine(object):
+            downloader = MockedDownloader()
+            fake_spider_closed_result = None
+
+            def close_spider(self, spider, reason):
+                self.fake_spider_closed_result = (spider, reason)
+
+        crawler = get_crawler(spider, settings)
+        crawler.engine = MockedEngine()
+        return crawler
